17ffa588a51e2b2814c198b11c45a14132900d74
Hugo Cavet <hcavet@c1r15s7.42lausanne.ch>
2025-06-12 14:00:08 +0200

    Updated phonebook to trim user input

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ego <ego@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: hcavet <hcavet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 16:16:11 by ego               #+#    #+#             */
-/*   Updated: 2025/06/11 14:51:40 by ego              ###   ########.fr       */
+/*   Updated: 2025/06/12 13:55:49 by hcavet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
+
+/**
+ * @brief Trims leading and trailing whitespace characters from a string.
+ * 
+ * @param s The input string to trim.
+ * 
+ * @return A trimmed copy of the input string.
+ */
+static std::string	trim(const std::string &s)
+{
+	const std::string	ws = " \t\n\v\f\r";
+	size_t				start = s.find_first_not_of(ws);
+	size_t				end = s.find_last_not_of(ws);
+
+	if (start == std::string::npos)
+		return ("");
+	return (s.substr(start, end - start + 1));
+}
+
 
 /**
  * @brief The main function of the PhoneBook program.
@@ -32,7 +51,7 @@ int	main(void)
 		phonebook.print_prompt();
 		if (!std::getline(std::cin, input))
 			break ;
-		if (!phonebook.handle_command(input))
+		if (!phonebook.handle_command(trim(input)))
 			break ;
 	}
 	std::cout << COLOR_B << "\nExited PhoneBook.\n" << C_RESET;
