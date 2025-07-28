9303062337b9c93d7309ef9d2ef12dd8340e51e0
Ego8316 <fuusukew@gmail.com>
2025-07-28 14:18:32 +0200

    ex01 done

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ego <ego@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 16:53:20 by ego               #+#    #+#             */
-/*   Updated: 2025/06/13 18:57:41 by ego              ###   ########.fr       */
+/*   Updated: 2025/07/28 14:14:49 by ego              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@ int main(void)
 {
 	std::cout << "--- Creating ScavTrap A (SC4V-TP) ---" << std::endl;
 	ScavTrap A("SC4V-TP");
+	A.guardGate();
+	A.attack("intruder");
 	return (0);
 }
