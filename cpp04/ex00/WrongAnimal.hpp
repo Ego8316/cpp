ee6b4da210c6bd287ad900b58520710c593d0744
Ego8316 <fuusukew@gmail.com>
2025-08-19 16:54:43 +0200

    corrected ex00

--- cpp04/ex00/WrongAnimal.hpp
+++ cpp04/ex00/WrongAnimal.hpp
@@ -6,7 +6,7 @@
 /*   By: ego <ego@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 13:54:49 by ego               #+#    #+#             */
-/*   Updated: 2025/08/19 15:34:18 by ego              ###   ########.fr       */
+/*   Updated: 2025/08/19 16:54:10 by ego              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ class	WrongAnimal
 		WrongAnimal(void);
 		WrongAnimal(std::string const &type);
 		WrongAnimal(WrongAnimal const &src);
-		virtual ~WrongAnimal(void);
+		~WrongAnimal(void);
 
 		WrongAnimal	&operator=(WrongAnimal const &src);
 
-		virtual void	makeSound(void) const;
+		void	makeSound(void) const;
 		std::string		getType(void) const;
 };
 
