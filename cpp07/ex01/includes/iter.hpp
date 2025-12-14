a21e6bb935a5a7c78a25675158053f7a1aae8ac9
Ego8316 <fuusukew@gmail.com>
2025-12-14 18:13:50 +0100

    cpp 07 ex00 and ex01 working

--- cpp07/ex01/includes/iter.hpp
+++ cpp07/ex01/includes/iter.hpp
@@ -6,13 +6,44 @@
 /*   By: ego <ego@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 17:12:34 by ego               #+#    #+#             */
-/*   Updated: 2025/12/14 17:34:33 by ego              ###   ########.fr       */
+/*   Updated: 2025/12/14 18:12:11 by ego              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-template <typename T> void	iter(T &array, const size_t length, (*f)(T))
+#include <sstream>
+#include <iostream>
+
+template <typename T> std::string	arrayToStr(T *array, const size_t len)
 {
-	
+	std::ostringstream	oss;
+
+	oss << "[";
+	for (size_t i = 0; i < len; ++i)
+	{
+		oss << array[i];
+		if (i != len - 1)
+			oss << ", ";
+	}
+	oss << "]";
+	return (oss.str());
+}
+
+template <typename T> void	iter(T *array, const size_t len, void (*func)(T &))
+{
+	if (array == NULL || func == NULL)
+		return ;
+	for (size_t i = 0; i < len; ++i)
+		func(array[i]);
+	return ;
+}
+
+template <typename T> void	iter(const T *array, const size_t len, void (*func)(const T &))
+{
+	if (array == NULL || func == NULL)
+		return ;
+	for (size_t i = 0; i < len; ++i)
+		func(array[i]);
+	return ;
 }
--- cpp07/ex01/srcs/main.cpp
+++ cpp07/ex01/srcs/main.cpp
@@ -6,31 +6,54 @@
 /*   By: ego <ego@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 14:04:48 by ego               #+#    #+#             */
-/*   Updated: 2025/12/14 17:28:37 by ego              ###   ########.fr       */
+/*   Updated: 2025/12/14 18:08:30 by ego              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-#include "whatever.hpp"
+#include "iter.hpp"
+
+void	mult(int &i)
+{
+	i *= 2;
+	return ;
+}
+
+void	mult_const(const int &i)
+{
+	(void)i;
+	return ;
+}
+
+void	toupper(char &c)
+{
+	if (c >= 'a' &&  c <= 'z')
+		c += 'A' - 'a';
+	return ;
+}
+
+void	toupper_const(const char &c)
+{
+	(void)c;
+	return ;
+}
 
 int	main(void)
 {
-	int	a = 2;
-	int	b = 3;
+	int		array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	char	str[] = "salut ca va";
 
-	::swap(a, b);
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
-	std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
+	std::cout << "Array before iter: " << ::arrayToStr(array, 10) << std::endl;
+	::iter(array, 10, mult);
+	std::cout << "Array after iter: " << ::arrayToStr(array, 10) << std::endl;
+	::iter(array, 10, mult_const);
+	std::cout << "Array after iter const: " << ::arrayToStr(array, 10) << std::endl;
 
-	std::string	c = "chaine1";
-	std::string	d = "chaine2";
-
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
-	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
-	
+	std::cout << "String before iter: " << str << std::endl;
+	::iter(str, sizeof(str), toupper);
+	std::cout << "String after iter: " << str << std::endl;
+	::iter(str, sizeof(str), toupper_const);
+	std::cout << "String after iter const: " << str << std::endl;
 	return (0);
 }
